334d7222430a72d590349e7923e246804c6b1ac2
DavValCal <davidvalenciac01@gmail.com>
2023-12-05 11:50:24 +0100

    ino file modification for merging

--- src/agl_arduino/AGL_PwmSerial/AGL_PwmSerial.ino
+++ src/agl_arduino/AGL_PwmSerial/AGL_PwmSerial.ino
@@ -1,6 +1,5 @@
 // Library includes
 #include <math.h>
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,21 +10,12 @@ volatile float currentInterruptionTimeR = 0;
 volatile float pastInterruptionTimeR = 0;
 volatile float deltaInterruptionTimeR = 0;
 volatile float currentTime = 0;
-=======
-
-// Interruption times and speed calculation variables for right servo.
-volatile double currentInterruptionTimeR = 0;
-volatile double pastInterruptionTimeR = 0;
-volatile double deltaInterruptionTimeR = 0;
-volatile double currentTime = 0;
->>>>>>> development
 
 int IN3_MR = 5;
 int IN4_MR = 4;
 
 int encoderR = 19; // Right encoder pin.
 int rWheel = 11;   // PWM pin for right servo, right wheel.
-<<<<<<< HEAD
 
 float rFrequency = 0; // Interruption frequency for right wheel.
 float Vr = 0;         // Linear Velocity Right.
@@ -45,25 +35,6 @@ bool firstEncoderRead = true;                     // Boolean for first encoder r
 int x = 0;
 String ros2Data = "";
 char buffer[MAX_SIZE_BUFFER];
-=======
-
-double rFrequency = 0; // Interruption frequency for right wheel.
-double Wr = 0;         // Angular Velocity Right.
-double Vr = 0;         // Linear Velocity Right.
-
-double N = 24.0;                                  // Reads encoder.
-double gear = 74.83;                              // Gear ratio 74.83:1.
-float diameter = 10;                              // Wheel diamenter in cm.
-float length = 35;                                // Lenght between to wheels, necessary for two wheels speed control.
-int tickCounter = 3;                              // Times per tick in encoder for which we'll calculate speed in order to reduce signal noise.
-int CR = 0;                                       // Counter < tickCounter. If equal calculate speed.
-int vecSize = 10;                                 // Vector size for rVector.
-float rVector[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Mean time frequency calculation vector for reducing signal noise.
-int pwr = 0;                                      // Variable for predicted PWM value for servo.
-int encoderCounterFilter = 0;                     // Variable for filtering in vector size vecSize.
-bool firstEncoderRead = true;                     // Boolean for first encoder read control.
-
->>>>>>> development
 void setup()
 {
 
@@ -76,13 +47,9 @@ void setup()
   digitalWrite(IN4_MR, LOW);
 
   attachInterrupt(digitalPinToInterrupt(encoderR), REncoder, FALLING); // PIN Interruption set to detect a falling flank in encoderR pin, it'll activate REncoder function.
-<<<<<<< HEAD
   Serial.begin(115200);  // Start of Serial communication.
   Serial.setTimeout(1);
                                                
-=======
-  Serial.begin(9600);                                                  // Start of Serial communication.
->>>>>>> development
 }
 
 void REncoder() // Interruption function for right wheel encoder.
@@ -132,7 +99,6 @@ void REncoder() // Interruption function for right wheel encoder.
 void loop()
 {
 
-<<<<<<< HEAD
   while (Serial.available() > 0){
     // Transform recieved serial with division by 100.0 .
     ros2Data = Serial.readString();
@@ -140,11 +106,6 @@ void loop()
   currentTime = millis();
 
   float realDelta = (currentTime - pastInterruptionTimeR);
-=======
-  currentTime = millis();
-
-  double realDelta = (currentTime - pastInterruptionTimeR);
->>>>>>> development
 
   if (realDelta >= 8 * tickCounter) // At 0 velocity our frequency should be 0.
   {
@@ -153,7 +114,6 @@ void loop()
 
   Wr = (tickCounter * ((2 * 3.141516) / N) * rFrequency) / gear; // Angular speed Rad/s.
   Vr = Wr * (diameter / 2);                                      // Linear speed cm/s.
-<<<<<<< HEAD
   float rounded_downWr = floorf(Wr * 100);                       // Rounding to two decimals our speeds
   float rounded_downVr = floorf(Vr * 100);
   int intWr = (int)rounded_downWr;                               // Getting the integers from our speeds, should divide by 100 in ros code.
@@ -178,21 +138,6 @@ void loop()
     float e = wt - Wr;
     float eintegral = eintegral + e * deltaInterruptionTimeR;
     float u = kp * e + ki * eintegral;
-=======
-
-  // Objective velocity in rad/s with 0-6.17 range where ~4.2 is the value in which the servo is functional.
-  // If velocity is set between [0;4.2] hardcode 0 pwm as the polinomic regression applied to predict pwm values from angular velocities cannot deal with servo behavior for low PWMs (no speed until 100 PWM).
-  double wt = 4.3;
-
-  if (wt >= 4.2)
-  {
-    // Angular velocity to PWM transformation via polinomial regression.
-    double kp = 1;
-    double ki = 1;
-    double e = wt - Wr;
-    double eintegral = eintegral + e * deltaInterruptionTimeR;
-    double u = kp * e + ki * eintegral;
->>>>>>> development
     
     if (u > 0)
     {
@@ -220,19 +165,7 @@ void loop()
   //      delay(1000);
   //    }
 
-<<<<<<< HEAD
 //  ////////////////////////////////////////////////////////////////////
-=======
-  ////////////////////////////////////////////////////////////////////
-
-  Serial.print(wt);
-
-  Serial.print(" ");
-
-  Serial.print(Wr);
-
-  Serial.println();
->>>>>>> development
 
   analogWrite(rWheel, pwr); // PWM applied to right servo.
 }
